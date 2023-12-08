d095623619cd10bf150099159f5ec2a338028fd4
Alex Inga <alexinga98@gmail.com>
2023-12-07 20:34:17 -0500

    modifying store and main code

--- src/store.js
+++ src/store.js
@@ -1,3 +1,14 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import userReducer from "./features/user/userSlice";
+// import cartReducer from "./features/cart/cartSlice";
+
+// const store = configureStore({
+//   reducer: {
+//     user: userReducer,
+//     cart: cartReducer,
+//   },
+// });
+// export default store;
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 import cartReducer from "./features/cart/cartSlice";
@@ -8,4 +19,5 @@ const store = configureStore({
     cart: cartReducer,
   },
 });
+
 export default store;
